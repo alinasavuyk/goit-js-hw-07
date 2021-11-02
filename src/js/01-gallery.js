8826008dacd0bf9ea0460eeb2ffe4656e24ad0f5
alinasavuyk <87060358+alinasavuyk@users.noreply.github.com>
2021-11-02 20:24:42 +0200

    1

---
+++ src/js/01-gallery.js
@@ -0,0 +1,56 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+const cardsMarkup = createColorCardsMarkup(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createColorCardsMarkup(galleryItems) {
+  return galleryItems
+    .map(({preview, original, description }) => {
+      return `
+      <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+    })
+    .join('');
+}
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+    const isColorSwatchEl = evt.target.classList.contains('gallery__image');
+   
+    if (!isColorSwatchEl) {
+      return;
+    }
+    const swatchEl = evt.target.dataset.source;
+    console.log(swatchEl)
+   
+    
+
+
+    const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${swatchEl}" width="800" height="600">
+    
+    </div>
+       
+`)
+
+instance.show()
+
+
+  }
